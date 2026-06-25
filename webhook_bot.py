ee6abaf23f93d63d55dae78344d9a88aa0b51a72
Максим Максимов <maksimmaksimov@MacBook-Air-Maksim.local>
2026-06-25 06:14:57 +0300

    qwe

--- webhook_bot.py
+++ webhook_bot.py
@@ -10,7 +10,7 @@ from config import TOKEN
 from handlers.profile import profile, edit_profile_start, handle_profile_input, editing_state
 from handlers.favorites import add_to_favorites, view_favorites, fav_to_cart, fav_remove
 from handlers.cart import (
-    add_to_cart, cart_select_size, cart_confirm_quantity, view_cart,
+    add_to_cart, cart_select_size, view_cart,
     cart_increase, cart_decrease, cart_remove, view_cart_from_profile, view_cart_from_product, cart_remove_group, cart_select_attr, cart_confirm_add
 )
 from handlers.order import order_start, order_handle, order_select_size, back_to_size, show_order_form, order_select_attr, order_confirm
@@ -73,7 +73,6 @@ def main() -> None:
     # --- КОРЗИНА (ВСЕ ОБРАБОТЧИКИ!) ---
     application.add_handler(CallbackQueryHandler(add_to_cart, pattern="^cart_add_"))
     application.add_handler(CallbackQueryHandler(cart_select_size, pattern="^cart_size_"))
-    application.add_handler(CallbackQueryHandler(cart_confirm_quantity, pattern="^cart_qty_"))
     application.add_handler(CallbackQueryHandler(view_cart, pattern="^view_cart$"))
     application.add_handler(CallbackQueryHandler(view_cart_from_product, pattern="^view_cart_from_product$"))
     application.add_handler(CallbackQueryHandler(view_cart_from_profile, pattern="^view_cart_from_profile$"))
